ed715db4df24790213b6423bd16a13289c4cdc2c
wywnc <73680478+wywnc@users.noreply.github.com>
2020-10-30 12:58:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
         <title>itf lab 63070154</title>
     </head>
     <body>
+        <meta charset="UTF-8">
         <p>Wanatchporn Silaruam (Ria)</p>
         <img src="ramen2.jpg">
         <img src="ramen1.png">
         <br>
         <a href="https://eg44235.wixsite.com/artportfolio">my artworks</a>
+        <p>ตำรวจจับผู้ร้ายได้คนหนึ่ง</p>
+        <p>กำลังจะพาไปโรงพัก</p>
+        <p>แต่เผอิญตำรวจอยากสูบบุหรี่ ผู้ร้ายจึงบอกว่า</p>
+        <p>ผู้ร้าย: เดี๋ยวผมไปซื้อให้ครับ</p>
+        <p>ตำรวจ: แกคิดว่าฉันโง่รึไง ถ้าแกไปซื้อแกก็หนีสิ</p> 
+        <p>แกรออยู่ตรงนี้แหละเดี๋ยวฉันไปซื้อเอง</p>
     </body>
 </html>
